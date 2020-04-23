8830153bb1134fd6d4175a1b5639c14105567415
RasmusTh30wl <61754229+RasmusTh30wl@users.noreply.github.com>
2020-04-23 23:37:08 +0200

    Update App.java

--- Laboratorium/Zajęcia 5/Zadanie 2 i 3/App.java
+++ Laboratorium/Zajęcia 5/Zadanie 2 i 3/App.java
@@ -4,6 +4,8 @@ package org.example;
  * Hello world!
  *
  */
+
+//wersja z tablicą (ekosystem jest ograniczony z góry liczbą zwierząt), czyli rozwiązanie nie do końca poprawne
 public class App 
 {
     public static void main( String[] args )
@@ -19,6 +21,30 @@ public class App
         System.out.println("------------------");
 
         for(int i=0;i<7;i++) {
+            eco.print();
+            eco.birth();
+            eco.addAge();
+        }
+    }
+}
+
+
+//wersja z ArrayListą (ekosystem nie jest ograniczony z góry liczbą zwierząt)
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ArrayList <Animal> animals= new ArrayList<>();
+        //animals[0]=new Animal();
+        animals.add(new Animal(Species.BEAR,2,7,true));
+        //animals[4]= new Animal(Species.BEAR,3,7,false);
+        animals.add(new Animal(Species.BEAR,2,7,false));
+        //animals[3]= new Animal(Species.BEAR,3,7,false);
+
+        Ecosystem eco=new Ecosystem(animals);
+        System.out.println("------------------");
+
+        for(int i=0;i<5;i++) {
             eco.print();
             eco.birth();
             eco.addAge();
