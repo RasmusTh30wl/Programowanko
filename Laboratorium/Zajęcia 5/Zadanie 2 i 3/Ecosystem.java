2b15b5c3f2edb11a788e2cb76d3ec2a0be3650e5
RasmusTh30wl <61754229+RasmusTh30wl@users.noreply.github.com>
2020-04-23 23:35:44 +0200

    Update Ecosystem.java

--- Laboratorium/Zajęcia 5/Zadanie 2 i 3/Ecosystem.java
+++ Laboratorium/Zajęcia 5/Zadanie 2 i 3/Ecosystem.java
@@ -1,5 +1,7 @@
 package org.example;
 
+
+//wersja z tablicą (ekosystem ma ograniczoną z góry liczbę zwierząt)
 public class Ecosystem {
     private Animal[] animals;
     public Ecosystem(Animal[] animals ){
@@ -51,8 +53,52 @@ public class Ecosystem {
 
             }
         }
+    }
+}
 
 
+//wersja z ArrayListą (ekosystem ma nieograniczoną z góry liczbę zwierząt)
+public class Ecosystem {
+    private ArrayList<Animal> animals;
+    private Iterator itr;
+    public Ecosystem(ArrayList<Animal> animals){
+        this.animals=new ArrayList<>(animals);
+    }
 
+
+    public void addAge(){
+        this.itr=animals.iterator();
+        while(itr.hasNext()){
+            Animal a=(Animal)itr.next();
+            if(!a.addAge())
+                itr.remove();
+        }
+    }
+    public void print(){
+        System.out.println("\nEcosystem:");
+        int count=0;
+        for (Animal animal : animals) {
+            System.out.println(animal.getName() + " " + animal.getAge() + " " + animal.getMaxAge() + " " + animal.getSexSymbol());
+            count++;
+        }
+        if(count==0) System.out.println("i nie było już nikogo");
+    }
+    public void birth(){
+        int[][] isPair=new int[Species.elements()][2];
+
+        for (Animal animal : animals) {
+            if (animal.getSex() && animal.getAge() >= 1) {
+                isPair[animal.getName().getID()][0]++;
+            } else if (!animal.getSex() && animal.getAge() >= 1) {
+                isPair[animal.getName().getID()][1]++;
+            }
+        }
+        for(int i=0; i<Species.elements(); i++){
+            int min=Math.min(isPair[i][0],isPair[i][1]);
+            if(min!=0){
+                for(int j=0;j<min;j++)
+                    animals.add(new Animal(i));
+            }
+        }
     }
 }
