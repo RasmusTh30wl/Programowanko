a164bdc8ac858d6174b411fdb5534234c1ed0fa4
RasmusTh30wl <61754229+RasmusTh30wl@users.noreply.github.com>
2020-04-29 11:19:37 +0200

    Update User.java

--- Laboratorium/Zajęcia 2/User.java
+++ Laboratorium/Zajęcia 2/User.java
@@ -1,3 +1,11 @@
+
+/*
+Zadanie 1.
+Napisz klasę User posiadającą prywatne pola login i password typu string. Napisz konstruktor przyjmujący obie wartości. 
+Napisz metodę authorize, która przyjmie w argumentach dwa napisy i sprawdzi, czy zgadzają się z polami w klasie. 
+Napisz metodę print wypisującą login i password użytkownika.
+*/
+
 package com.company;
 
 public class User {
