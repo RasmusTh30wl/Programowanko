345cf2757ca0937f6ae6b15e0ae3cdce859b407e
RasmusTh30wl <61754229+RasmusTh30wl@users.noreply.github.com>
2020-04-29 11:30:09 +0200

    Update Triangle.java

--- Laboratorium/Zajęcia 1/Triangle.java
+++ Laboratorium/Zajęcia 1/Triangle.java
@@ -1,3 +1,12 @@
+
+/*
+Zadanie 6.
+W osobnym pliku zdefiniuj klasę Triangle. Powinna ona posiadać trzy prywatne pola typu Point określające położenie punktów trójkąta. 
+Napisz konstruktor, który przyjmie jako argumenty trzy obiekty typu Point. 
+Napisz publiczne metody perimeter zwracający obwód trójkąta oraz area zwracający pole trójkąta, oba w typie float. 
+Przy obliczaniu pola skorzystaj z wzoru Herona: √p(p−a)(p−b)(p−c), gdzie p jest połową obwodu.
+*/
+
 package org.example;
 
 public class Triangle {
