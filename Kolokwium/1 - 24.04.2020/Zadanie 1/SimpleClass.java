4b5a8f86242359e14637c35880a7b1f1a9f70b57
RasmusTh30wl <61754229+RasmusTh30wl@users.noreply.github.com>
2020-04-29 13:56:17 +0200

    Update SimpleClass.java

--- Kolokwium/1 - 24.04.2020/Zadanie 1/SimpleClass.java
+++ Kolokwium/1 - 24.04.2020/Zadanie 1/SimpleClass.java
@@ -3,17 +3,18 @@ public class SimpleClass {
     public SimpleClass(String str){
         this.str=str;
     }
+    
+    
+    //wersja bez StringBuildera
     public void SimpleMethod(char a, char b){
-        String tab[]=str.split(" ");
         String result="";
-        for (String x:tab) {
-            String z =x.charAt(0)+"";
+        for (String x:str.split(" ")) {
             if (x.toLowerCase().startsWith(String.valueOf(a))&&x.toLowerCase().endsWith(String.valueOf(b))){
+                result+=x.charAt(0);
                 for (int i = 0; i <x.length()-2 ; i++) {
-                    z+='*';
+                    result+='*';
                 }
-                z+=b;
-                result+=z;
+                result+=b;
             }
             else{
                 result+=x;
@@ -25,18 +26,47 @@ public class SimpleClass {
     }
 
     public void SimpleMethod2(){
-        String tab[]=str.split(" ");
         String result="";
-        for (String x:tab) {
-            String z=x.toUpperCase().charAt(0)+"";
+        for (String x:str.split(" ")) {
+            result+=x.toUpperCase().charAt(0);
             if (x.length()>1) {
-                for (int i = 1; i < x.length() - 1; i++) {
-                    z += x.charAt(i);
-                }
-                z += x.toUpperCase().charAt(x.length() - 1);
+                result += x.substring(1, x.length() - 1);
+                result += x.toUpperCase().charAt(x.length() - 1);
             }
-            result += z + " ";
+            result += " ";
         }
         System.out.println(result);
+    }
+
+    
+    //wersja z StringBuilderem
+    public void metoda1(char a, char b) {
+        StringBuilder builder1 = new StringBuilder();
+        for (String s : str.split(" ")) {
+            if (s.toLowerCase().startsWith(String.valueOf(a))&&s.toLowerCase().endsWith(String.valueOf(b))) {
+                builder1.append(s.charAt(0));
+                for (int i = 0; i < s.length() - 2; i++) {
+                    builder1.append("*");
+                }
+                builder1.append(s.charAt(s.length() - 1));
+            } else {
+                builder1.append(s);
+            }
+            builder1.append(" ");
+        }
+        System.out.println(builder1);
+    }
+
+    public void metoda2() {
+        StringBuilder builder1 = new StringBuilder();
+        for (String s : str.split(" ")) {
+            builder1.append(s.toUpperCase().charAt(0));
+            if (s.length() > 1) {
+                builder1.append(s.substring(1, s.length() - 1));
+                builder1.append(s.toUpperCase().charAt(s.length() - 1));
+            }
+            builder1.append(" ");
+        }
+        System.out.println(builder1);
     }
 }
