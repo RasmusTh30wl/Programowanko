298f9815be2f59f614dec1921882d2f7bf35d16f
RasmusTh30wl <61754229+RasmusTh30wl@users.noreply.github.com>
2020-04-29 11:28:30 +0200

    Update Point.java

--- Laboratorium/Zajęcia 1/Point.java
+++ Laboratorium/Zajęcia 1/Point.java
@@ -1,3 +1,30 @@
+
+/*
+Zadanie 1.
+Napisz klasę Point posiadającą dwa pola prywatne typu float. Napisz akcesory do tych pól.
+*/
+
+/*
+Zadanie 2a.
+Do klasy Point dopisz metodę move, która przyjmie dwa argumenty typu float - x i y, która przesunie punkt o zadany wektor.
+*/
+
+/*
+Zadanie 2b.
+Przeciąż metodę move, aby mogła przyjmować w argumencie obiekt klasy Point, którego pola x, y traktowane są jako składowe wektora.
+*/
+
+/*
+Zadanie 3.
+Usuń z klasy Point settery. Napisz konstruktor, który przyjmie dwa argumenty typu float i ustawi ich wartości polom x i y.
+*/
+
+/*
+Zadanie 4.
+Napisz metodę distance, która przyjmie jako argument obiekt klasy Point, a następnie zwróci odległość między punktami 
+wyrażoną za pomocą typu double.
+*/
+
 package org.example;
 
 class Point{
